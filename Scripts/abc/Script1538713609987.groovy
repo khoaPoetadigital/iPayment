0738a9a3c1071f30413461f5eae1ff4431f2641a
DESKTOP-V4NEK1F\Khoa <khoa@poetadigital.com>
2018-10-05 15:34:22 +0700

    Create test cases folder for Digital Loyalty

--- Scripts/abc/Script1538713609987.groovy
+++ Scripts/abc/Script1538713609987.groovy
@@ -13,11 +13,5 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.acceptAlert()
 
-Mobile.checkElement(findTestObject(null), 0)
-
-WebUI.acceptAlert()
-
-WebUI.acceptAlert()
 
