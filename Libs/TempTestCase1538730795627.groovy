9379b62e55be78a023165201e571d42f822f08e7
DESKTOP-V4NEK1F\Khoa <khoa@poetadigital.com>
2018-10-05 16:14:42 +0700

    Update tc

---
+++ Libs/TempTestCase1538730795627.groovy
@@ -0,0 +1,23 @@
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.logging.KeywordLogger
+import groovy.lang.MissingPropertyException
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.webui.contribution.WebUiDriverCleaner
+import com.kms.katalon.core.mobile.contribution.MobileDriverCleaner
+import com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner
+
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+
+
+RunConfiguration.setExecutionSettingFile('C:\\Users\\Khoa\\AppData\\Local\\Temp\\Katalon\\Test Cases\\Digital Loyalty\\Login\\Verify that clicking to Digital Punch Card will redirect to Digital Loyalty page\\20181005_161315\\execution.properties')
+
+TestCaseMain.beforeStart()
+
+        TestCaseMain.runTestCase('Test Cases/Digital Loyalty/Login/Verify that clicking to Digital Punch Card will redirect to Digital Loyalty page', new TestCaseBinding('Test Cases/Digital Loyalty/Login/Verify that clicking to Digital Punch Card will redirect to Digital Loyalty page',[:]), FailureHandling.STOP_ON_FAILURE , false)
+    
---
+++ Object Repository/Pirq Portal home/nav_DigitalPunchCard_Portal_Homepage.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>//a[@href=&quot;/digital-loyalty/main&quot;]</description>
+   <name>nav_DigitalPunchCard_Portal_Homepage</name>
+   <tag></tag>
+   <elementGuidId>6dc20889-878a-4812-96b0-5a3224722886</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@href=&quot;/digital-loyalty/main&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
